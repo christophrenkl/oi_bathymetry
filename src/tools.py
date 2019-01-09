e409d776d9f78477b326e258342cf826803d0e18
Christoph Renkl <christoph.renkl@dal.ca>
2019-01-09 11:56:21 -0400

    Add function to compute Cartesian coordinates from longitude/latitude

--- src/tools.py
+++ src/tools.py
@@ -14,3 +14,17 @@ def read_grid(fname):
     grd = xr.open_dataset(fname)
 
     return grd
+
+
+def ll2xyz(lon, lat, R=6371000):
+    """
+    Calculates coordinates of a point on a sphere with radius R
+    """
+    lon_r = np.radians(lon)
+    lat_r = np.radians(lat)
+
+    x =  R * np.cos(lat_r) * np.cos(lon_r)
+    y = R * np.cos(lat_r) * np.sin(lon_r)
+    z = R * np.sin(lat_r)
+
+    return x,y,z
